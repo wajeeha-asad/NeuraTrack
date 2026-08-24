2e583192702958ded28dede220a2564c0a1504e3
wajeeha-asad <techwithwajeeha@gmail.com>
2026-08-24 13:47:37 +0500

    Allow production frontend in CORS

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "https://neuratrack-three.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
